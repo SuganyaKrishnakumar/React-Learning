8901d1da21f18fb838c52ee6616496c9a6f7cfdf
Suganya K <suganya.k@inxsasia.com>
2021-06-02 14:49:02 +0530

    basics of components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import ExpenseItem from './components/ExpenseItem';
-function App() {
+import React from 'react';
+import Expenses from './components/Expenses/Expenses';
+
+const App =() => {
   const expenses = [
     {
       id: 'e1',
@@ -23,13 +23,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
-       <h2> Lets start </h2>
-       <ExpenseItem  title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-       <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-       <ExpenseItem  title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-       <ExpenseItem  title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
